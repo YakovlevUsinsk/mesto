db55e27a8321bb92fccdf7545c9411bdb9898e5e
Mikhail Yakovlev <misha.j.usinsk@gmail.com>
2022-04-08 22:53:04 +0300

    ver 2.0

--- scripts/index.js
+++ scripts/index.js
@@ -4,24 +4,24 @@ let profileInterest = profileForm.querySelector ('.profile__info-interest');//с
 let btnEdit = profileForm.querySelector ('.profile__btn-edit');//выделение в переменную кнопки edit
 let popupBlock = document.querySelector ('.popup');//выделение блока popup в переменную
 let clsPopup = popupBlock.querySelector ('.popup__btn-close');//выделение в переменную кнопки close
-
-function toogleForm () {//фунция переключения класса 
-    popupBlock.classList.toggle ('popup_opened');
-}
-
-clsPopup.addEventListener ('click', toogleForm);
-btnEdit.addEventListener ('click', toogleForm);
-
 let formElement = popupBlock.querySelector ('.popup__form');//форма в DOM
-let dataField =formElement.querySelectorAll ('.popup__field');// массив значения value
+let dataField =formElement.querySelectorAll ('.popup__input-text_field');// массив значения value
+
+function toogleForm () {//фунция переключения класса
+  dataField[0].placeholder = profileName.textContent;
+  dataField[1].placeholder = profileInterest.textContent;
+  popupBlock.classList.toggle ('popup_opened');
+}
 
 function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                                                // Так мы можем определить свою логику отправки.
-                                                // О том, как это делать, расскажем позже.
-    profileName.textContent = dataField[0].value; //присвоение 
-    profileInterest.textContent = dataField[1].value;
-    toogleForm ();
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+  // Так мы можем определить свою логику отправки.
+  // О том, как это делать, расскажем позже.
+  profileName.textContent = dataField[0].value; //присвоение
+  profileInterest.textContent = dataField[1].value;
+  toogleForm ();
 }
 
+btnEdit.addEventListener ('click', toogleForm);
+clsPopup.addEventListener ('click', toogleForm);
 formElement.addEventListener('submit', formSubmitHandler); //обработка события submit

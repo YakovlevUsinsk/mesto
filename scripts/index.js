bf7d1011b9413b2ee831cb8323c6da31d97ba8c6
Mikhail Yakovlev <misha.j.usinsk@gmail.com>
2022-04-13 22:58:05 +0300

    разработана функция добавления карточки

--- scripts/index.js
+++ scripts/index.js
@@ -27,4 +27,31 @@ function formSubmitHandler (evt) {
 btnEdit.addEventListener ('click', toogleForm);
 clsPopup.addEventListener ('click', toogleForm);
 formElement.addEventListener('submit', formSubmitHandler); //обработка события submit
-// 
+//  Новая работа
+
+const initialCards = [
+  {name: 'Архыз', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'},
+  {name: 'Челябинская область', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'},
+  {name: 'Иваново', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'},
+  {name: 'Камчатка', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'},
+  {name: 'Холмогорский район', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'},
+  {name: 'Байкал', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'}
+];
+
+const templateElement = document.querySelector('.template').content;
+const containerCards = document.querySelector('.elements');
+
+function getCard (item){
+  const cardElement = templateElement.querySelector('.elements__cards').cloneNode(true);
+  const nameCard = cardElement.querySelector('.elements__name');
+  const imgCard = cardElement.querySelector('.elements__img');
+  imgCard.src = item.link;
+  nameCard.textContent = item.name;
+  containerCards.append(cardElement);
+  return cardElement;
+};
+
+function render () {
+  initialCards.map(getCard);
+}
+render ();

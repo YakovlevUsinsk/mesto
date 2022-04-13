b73193d4c9a2121b5ad01e35273dc4d25ed3bed7
Mikhail Yakovlev <misha.j.usinsk@gmail.com>
2022-04-13 16:52:09 +0300

    Добавление popup для ввода данных карточки

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,7 @@ let profileForm = document.querySelector ('.profile__info');
 let profileName = profileForm.querySelector ('.profile__info-name');//строка имени блока профиля
 let profileInterest = profileForm.querySelector ('.profile__info-interest');//строка интересов блока профиля
 let btnEdit = profileForm.querySelector ('.profile__btn-edit');//выделение в переменную кнопки edit
-let popupBlock = document.querySelector ('.popup');//выделение блока popup в переменную
+let popupBlock = document.querySelector ('.popup_users');//выделение блока popup в переменную
 let clsPopup = popupBlock.querySelector ('.popup__btn-close');//выделение в переменную кнопки close
 let formElement = popupBlock.querySelector ('.popup__form');//форма в DOM
 let popupValueName =formElement.querySelector ('.popup__input_value_name');// выделение в переменную input name формы popup
@@ -27,3 +27,4 @@ function formSubmitHandler (evt) {
 btnEdit.addEventListener ('click', toogleForm);
 clsPopup.addEventListener ('click', toogleForm);
 formElement.addEventListener('submit', formSubmitHandler); //обработка события submit
+// 
